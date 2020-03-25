6217d97fa79e92371fe695ef62157e33c347f9e1
wanxiangsheng <1401582905@qq.com>
2020-03-25 15:49:58 +0800

    public function destroy()

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,20 +19,20 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'name' => 'required|unique:users|max:50',
-            'email' => 'required|email|unique:users|max:255',
+        $this->validate($request, [
+            'name'     => 'required|unique:users|max:50',
+            'email'    => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
 
         $user = User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password),
         ]);
 
         Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
